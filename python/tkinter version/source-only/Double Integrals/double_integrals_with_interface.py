fd054aa2bf0d7a290d4ee76f250e89de8dcf0ec7
pavlos tzitzos <94289294+PavlosTzitzos@users.noreply.github.com>
2023-01-30 21:52:26 +0200

    Update double_integrals_with_interface.py

--- python/tkinter version/source-only/Double Integrals/double_integrals_with_interface.py
+++ python/tkinter version/source-only/Double Integrals/double_integrals_with_interface.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan  1 20:19:04 2022
-
-@author: user
-"""
+#import libraries :
 from sympy import exp
 from sympy import symbols
 from sympy import integrate
@@ -18,9 +13,17 @@ import tkinter as tk
 from sympy import latex
 import matplotlib.pyplot as plt
 
+#create the variable for cartesian or polar coordinates
 global pc
 
 def sym_compare(a,b):
+    """
+        compare two sympy symbols a b :
+        return value : condition
+        1 : a > b
+        -1: a < b
+        0 : a = b
+    """
     d = a-b
     if d.is_positive:
         return 1
@@ -30,13 +33,23 @@ def sym_compare(a,b):
         return 0
 
 def swap_values(a,b):
+    """
+        Swaps 2 values of variables a and b:
+        a_new <-- b_old
+        b_new <-- a_old
+        returns new values of a and b
+    """
     temp = a
     a = b
     b = temp
     return a, b
 
 def warning_one(a,b):
-    
+    """
+        A warning message appears when swap function is called
+        when a and b are : a > b
+        returns new values of a and b
+    """
     if sym_compare(a,b) ==1:
         Label(window,text="Warning! Inverting Inputs ...").place(x=400,y=0)
         a,b = swap_values(a,b)
@@ -45,9 +58,17 @@ def warning_one(a,b):
     return a,b
 
 def di_main_func(fnct,xa,xb,yc,yd,pc,ra,rb,tc,td,k1,k2,ax,by):
+    """
+    main function starts here:
+    first declare symbols that the script requires
+    then ask for input in terminal
+    """
     x, y, r, theta = symbols('x y r theta')
     x1, x2, y1, y2 = symbols('x1 x2 y1 y2')
     kx, ky, a, b = symbols('k1 k2 ax by')
+    """
+        from strings convert to sympy objects:
+    """
     f=parse_expr(fnct)
     x1=parse_expr(xa)
     x2=parse_expr(xb)
@@ -64,13 +85,25 @@ def di_main_func(fnct,xa,xb,yc,yd,pc,ra,rb,tc,td,k1,k2,ax,by):
     csx=400
     csy=20
     Label(window, text="Calculating Double Integral ...").place(x=csx,y=csy)
-    
-    if pc == 0:
+    """
+        Starting calculations:
+         main idea is to check for the cases that are explained in readme.md file
+         and then decide the series of integration
+        
+       Start with cartesian coordinates and then go to polar coordinates
+    """
+    if pc == 0: #cartesian coordinates
         if (x1.is_number==1 and x2.is_number==1 and y1.is_number==1 and y2.is_number==1):
+            """
+                case 1 from readme.md file
+            """
             x1,x2 = warning_one(x1,x2)
             y1,y2 = warning_one(y1,y2)
             I1 = integrate(f,(x,x1,x2))
             I = integrate(I1,(y,y1,y2))
+            """
+                after calculating create the latex for the answer :
+            """
             mathtext_demos = {
                 "Header demo":
                     r"$You~entered: \int ^{"+latex(y2)+"} _{"+latex(y1)+"} \int ^{"+latex(x2)+"} _{"+latex(x1)+"} "+latex(f)+" dx dy$",
@@ -85,9 +118,15 @@ def di_main_func(fnct,xa,xb,yc,yd,pc,ra,rb,tc,td,k1,k2,ax,by):
             }
             
         elif((x1.is_number==0 or x2.is_number==0) and y1.is_number==1 and y2.is_number==1):
+            """
+                case 2 or 3 from readme.md file
+            """
             y1,y2 = warning_one(y1,y2)
             I1 = integrate(f,(x,x1,x2))
             I = integrate(I1,(y,y1,y2))
+            """
+                after calculating create the latex for the answer :
+            """
             mathtext_demos = {
                 "Header demo":
                     r"$You~entered: \int ^{"+latex(y2)+"} _{"+latex(y1)+"} \int ^{"+latex(x2)+"} _{"+latex(x1)+"} "+latex(f)+" dx dy$",
@@ -102,9 +141,15 @@ def di_main_func(fnct,xa,xb,yc,yd,pc,ra,rb,tc,td,k1,k2,ax,by):
             }
             
         else:
+            """
+                case 4 or 5 from readme.md file
+            """
             x1,x2 = warning_one(x1,x2)
             I1 = integrate(f,(y,y1,y2))
             I = integrate(I1,(x,x1,x2))
+            """
+                after calculating create the latex for the answer :
+            """
             mathtext_demos = {
                 "Header demo":
                     r"$You~entered: \int ^{"+latex(x2)+"} _{"+latex(x1)+"} \int ^{"+latex(y2)+"} _{"+latex(y1)+"} "+latex(f)+" dy dx$",
@@ -119,9 +164,15 @@ def di_main_func(fnct,xa,xb,yc,yd,pc,ra,rb,tc,td,k1,k2,ax,by):
             }
             
     elif pc==1:
+        """
+            polar coordinates:
+        """
         fp = f.subs([(x, r*cos(theta)+kx),(y, r*sin(theta)+ky)])
         I1 = integrate(fp*r,(r,r1,r2))
         I = integrate(I1,(theta,theta1,theta2))
+        """
+            after calculating create the latex for the answer :
+        """
         mathtext_demos = {
             "Header demo":
                 r"$You~entered: \int ^{"+latex(theta2)+"} _{"+latex(theta1)+"} \int ^{"+latex(r2)+"} _{"+latex(r1)+"} "+latex(fp)+" \cdot r dr d $"
@@ -144,9 +195,15 @@ def di_main_func(fnct,xa,xb,yc,yd,pc,ra,rb,tc,td,k1,k2,ax,by):
         }
         
     elif pc==2:
+        """
+            elliptical coordinates:
+        """
         fp = f.subs([(x, a*r*cos(theta)+kx),(y, b*r*sin(theta)+ky)])
         I1 = integrate(fp*a*b*r,(r,r1,r2))
         I = integrate(I1,(theta,theta1,theta2))
+        """
+            after calculating create the latex for the answer :
+        """
         mathtext_demos = {
             "Header demo":
                 r"$You~entered: \int ^{"+latex(theta2)+"} _{"+latex(theta1)+"} \int ^{"+latex(r2)+"} _{"+latex(r1)+"} "+latex(f)+" \cdot a \cdot b \cdot r dr d $"
@@ -167,7 +224,9 @@ def di_main_func(fnct,xa,xb,yc,yd,pc,ra,rb,tc,td,k1,k2,ax,by):
             "which gives final result:":
                 r"$I_2 = "+latex(I)+" $",
         }
-        
+    """
+        After the calculations print then image with latex using matplotlib
+    """
     n_lines = len(mathtext_demos)
     
     # Colors used in Matplotlib online documentation.
@@ -213,21 +272,42 @@ def di_main_func(fnct,xa,xb,yc,yd,pc,ra,rb,tc,td,k1,k2,ax,by):
                     color=mpl_grey_rgb, fontsize=16)
 
     plt.show()
-    #print("hi")
 
 def btn_di_ec(event):
+    """
+        event :
+            button double integral elliptical coordinates is pressed last
+        changes the global variable pc value to 2
+    """
     global pc
     pc = 2
 
 def btn_di_pc(event):
+    """
+        event :
+            button double integral polar coordinates is pressed last
+            changes the global variable pc value to 1
+    """
     global pc
     pc = 1
 
 def btn_di_cc(event):
+    """
+        event :
+            button double integral cartesian coordinates is pressed last
+        changes the global variable pc value to 0
+    """
     global pc
     pc = 0
     
 def call_compute():
+    """After input arguments:
+       > function f(x,y)
+       > edges of integration: x1 <= x <= x2 , y1 <= y <= y2
+       > edges of polar
+       > edges of elliptical
+       call the main function: di_main_func()   
+    """
     ff = ent_func.get()
     a=ent_x1.get()
     b=ent_x2.get()
@@ -246,6 +326,10 @@ def call_compute():
     
     
 def call_reset():
+    """
+        A reset function that makes all fields values 0.
+        and length of axes of ellitical coordinates 1.
+    """
     ent_func.delete(0, 'end')
     ent_x1.delete(0,'end')
     ent_x2.delete(0,'end')
@@ -273,10 +357,22 @@ def call_reset():
     ent_a.insert(0,"1")
     ent_b.insert(0,"1")
 
-
+"""
+    main function:
+        create the window
+        difine dimentions
+        add title
+"""
 window = tk.Tk()
 window.geometry("800x400")
 window.title("Double Integrals Calculator")
+"""
+    create labels
+    declare variables of tk
+    create Entries
+    place them inside the window
+    add buttons for coordinates to change the global pc value
+"""
 Label(window, text="Give the function f(x,y) for integration:",font=("Arial",10)).place(x=0,y=1)
 var_f = tk.StringVar()
 var_x1 = tk.StringVar()
@@ -366,11 +462,19 @@ ent_b = Entry(window,textvariable=var_b)
 ent_b.place(x=180,y=a+20)
 ent_b.insert(0,"1")
 a=a+60
-
+"""
+    final buttons:
+        compute : to call compute function and start calculations
+        reset   : to call reset function 
+        exit    : to exit the application
+"""
 Button(window, text="Compute",command=call_compute).place(x=0,y=a)
 
 Button(window, text="Reset", command=call_reset).place(x=150, y=a)
 
 Button(window, text="Exit", command=window.destroy).place(x=300,y=a)
 
+"""
+    end the main loop :
+"""
 window.mainloop()
