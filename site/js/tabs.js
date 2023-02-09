20a8eaf4469a3a47400a393731e329ff586cc390
pavlos tzitzos <94289294+PavlosTzitzos@users.noreply.github.com>
2023-02-09 20:15:29 +0200

    Update tabs.js

--- site/js/tabs.js
+++ site/js/tabs.js
@@ -19,7 +19,7 @@ function integralType(evt, type) {
     evt.currentTarget.className += " active";
 }
 
-function coordinatesType(evt, cityName) {
+function doubleCoordinates(evt, coordinatesType) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
@@ -36,6 +36,48 @@ function coordinatesType(evt, cityName) {
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(coordinatesType).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function tripleCoordinates(evt, coordinatesType) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent2");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks2");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(coordinatesType).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function lineVFcurve(evt, curveType) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent2");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks2");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(curveType).style.display = "block";
   evt.currentTarget.className += " active";
 }
